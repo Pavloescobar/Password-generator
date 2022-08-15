c210373b1a3cd7643eea0184cab1b2a874764cf0
Pav <pavan_1994@hotmail.co.uk>
2022-08-15 19:44:52 +0100

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -10,7 +10,7 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-
+# Not required for now, but storing 3 user inputs as total int value
 N = nr_letters + nr_numbers + nr_symbols
 
 for letter in letters:
